b20cd543f346cff66fdfeb7dc4ac8cf17d1d867d
beesswag <beesswag@gmail.com>
2019-06-06 15:16:53 +0200

    Added (Add post and view functions)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Posts;
+use App\Post;
 use Auth;
 class HomeController extends Controller
 {
@@ -25,7 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = User::find(Auth::id());
+        $posts = $user->posts()->latest()->get();
+        return view('home')->with('posts', $posts);
     
     }
 
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use Auth;
 
 class PagesController extends Controller
 {   
-    //
+    public function addnewpost(){
+        return view('pages.addpost');
+    }
+
+    public function storepost(Request $request){
+        $newpost = new Post;
+        $newpost->user_id = Auth::id();
+        $newpost->post = $request->post;
+        $newpost->save();
+        return redirect()->back()->with('success', 'New Post added');
+    }
 }
