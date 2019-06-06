8ae94f0d079ddbcdf805ef6dd470a14aa51ab178
Ngiyabonga <kealebogagakenne@gmail.com>
2019-06-06 17:17:59 +0200

    delete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\facades\schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        schema::defaultStringLength('191');
     }
 }
