7e541c6b6a3a0def273db9c2d56362c8b168a844
Bisasa <jacobdimbo.com@gmail.com>
2019-06-07 11:54:55 +0200

    updating my current project

--- database/migrations/2019_06_07_085225_create_comments_table.php
+++ database/migrations/2019_06_07_085225_create_comments_table.php
@@ -13,15 +13,10 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('  comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('comment');
-<<<<<<< HEAD
             $table->integer('user_id');
-=======
-            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('post_id')->references('id')->on('posts')->onDelete('cascade');
->>>>>>> c5f9843c7a54ab832c9935117064a05d70381ef6
             $table->timestamps();
         });
     }
