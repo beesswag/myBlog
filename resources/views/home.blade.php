0388d6b7d833040e268f62b32a022f80a2e98f4a
Ngiyabonga <kealebogagakenne@gmail.com>
2019-06-07 10:17:57 +0200

    the delete function works

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,12 @@
                                 <tr>
                                     <td>{{$post->post}}</td>
                                     <td ><a href="{{route('edit',$post->id)}}"><button class="btn btn-default btn-primary"> Edit the post</button></a></td>
-                                    <td ><a href="{{route('destroy', $post->id)}}"><button class="btn btn-default btn-danger">Delete the post</button></a>
+                                    <td >
+                                        <form action="{{route('destroy', $post->id)}}" method="post">
+                                            {{method_field('DELETE')}}
+                                            @csrf
+                                            <a href=""><button class="btn btn-default btn-danger">Delete the post</button></a>
+                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
