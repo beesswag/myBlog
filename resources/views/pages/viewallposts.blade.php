fd55b6a381b9fd38deb33337602e0bc8af255aed
Morwaake <zigamorapedi@gmail.com>
2019-06-09 13:21:41 +0200

    edit button appears only for the one commented

--- resources/views/pages/viewallposts.blade.php
+++ resources/views/pages/viewallposts.blade.php
@@ -12,22 +12,26 @@
                 <Form action ="{{route('commenting')}}" method='post'>
                   {{csrf_field()}}
                   <!-- <label for="post">Comment</label><br> -->
-                  <div class="well mb-2"><input type="text" name="comment" placeholder="Comment"></div> 
+                  <div class="well mb-2"><input type="text" name="comment" placeholder="Comment"></div>
                   <input type="hidden" name="post_id" value="{{$memberposts->id}}">
                   <input type="submit" value="Add comment" class="btn btn-default btn-success">
                 </Form>
-                
+
                 <div class="card default mt-2 m-2 p-1">
                     <div class="well">
                         @foreach($postcomment as $comments)
                             @if($memberposts->id == $comments->post_id)
+                              @if($comments->user_id == Auth::id())
                                 <p style="font-size:13px">{{$memberposts->user->name}} : {{$comments->comment}} </p>
                                 <td ><a href="{{route('editcomment',$comments->id)}}"><button class="btn btn-default btn-primary"> Edit comment</button></a></td>
+                                @else
+                                  <p style="font-size:13px">{{$memberposts->user->name}} : {{$comments->comment}} </p>
+                                @endif
                             @endif
                         @endforeach
                     </div>
                 </div>
-                                
+
             </div>
         </div>
     @endforeach
